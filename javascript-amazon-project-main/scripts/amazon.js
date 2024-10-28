9d36b994342a9ddf2763596c55af16e3ecb73054
Debanshu111 <debanshubiswas111@gmail.com>
2024-10-28 20:50:33 +0530

    removed console and excess codes

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,42 +1,5 @@
-// console.log('hello');
-//data structures -- saving the data-->
-// const products = [
-//   {
-//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-//     rating: {
-//       stars: 4.5,
-//       count: 87,
-//     },
-//     priceCents: 1090,
-//   },
-//   {
-//     image: "images/products/intermediate-composite-basketball.jpg",
-//     name: "Intermediate Size Basketball",
-//     rating: {
-//       stars: 4.0,
-//       count: 127,
-//     },
-//     priceCents: 2095,
-//   },
-//   {
-//     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-//     name: "Adults Plain Cotton T-Shirt - 2 Pack",
-//     rating: {
-//       stars: 4.5,
-//       count: 56,
-//     },
-//     priceCents: 799,
-//   },
-// ];
-
-
-let productsHTML = ''; // created to combine all of it
-
-//looping through the array
-// `` signs are used to write html inside js file
+let productsHTML = '';
 products.forEach((itemsInAmazonForUsers) => {
-//   const html =
 productsHTML = productsHTML +
   `<div class="product-container"> 
           <div class="product-image-container">
@@ -84,16 +47,11 @@ productsHTML = productsHTML +
             Add to Cart
           </button>
         </div>`;
-        // console.log(html);
 });
-console.log(productsHTML);
 
-//To search/detect the class used in HTML
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-//Add to Cart fuction for all products that are selected
 document.querySelectorAll('.addToCart').forEach((button) => {
   button.addEventListener('click', () => {
-    // console.log('Added product')
   })
 });

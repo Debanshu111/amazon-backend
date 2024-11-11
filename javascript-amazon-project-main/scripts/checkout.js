71d662cc98947960ca9973b0fa58d8243e820d84
Debanshu111 <debanshubiswas111@gmail.com>
2024-11-11 19:09:37 +0530

    function used for priceCents

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = "";
 
@@ -34,7 +35,7 @@ cart.forEach((cartItem) => {
                 ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                ${(matchingProduct.priceCents / 100).toFixed(2)}</div>
+                ${formatCurrency(matchingProduct.priceCents)}</div>
                 <div class="product-quantity">
                   <span> Quantity: <span class="quantity-label">${
                     cartItem.quantity

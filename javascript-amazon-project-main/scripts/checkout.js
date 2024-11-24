3a029e4f6d96780b9c3c4e16312cba56709d764e
Debanshu111 <debanshubiswas111@gmail.com>
2024-11-25 01:46:41 +0530

    dynamic HTML for payment summary  and saved data

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,3 +2,4 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 renderOrderSummary(); //For regenerating all the Html = MVC = (model-view-controller)
+renderPaymentSummary();

37390c59bd7cbc423691077f5cb3e5b639cd2da3
Debanshu111 <debanshubiswas111@gmail.com>
2024-12-03 20:40:41 +0530

    fixed the value issue and inserted default cart

--- javascript-amazon-project-main/data/cart-oop.js
+++ javascript-amazon-project-main/data/cart-oop.js
@@ -9,7 +9,18 @@ const cart = {
         this.cartItems = JSON.parse(localStorage.getItem("cart-oop"));
 
         if (!this.cartItems) {
-            this.cartItems = [];
+            this.cartItems = [
+                {
+                    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                    quantity: 2,
+                    deliveryOptionId: "1",
+                },
+                {
+                    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                    quantity: 1,
+                    deliveryOptionId: "2",
+                },
+            ];
         }
     },
 
@@ -31,7 +42,7 @@ const cart = {
         const quantitySelector = document.querySelector(
             `.js-quantity-selector-${productId}`
         );
-        const quantity = Number(quantitySelector.value);
+        const quantity = Number(quantitySelector?.value);
         //Cart Item already in Cart?
         if (matchingItem) {
             matchingItem.quantity = matchingItem.quantity + quantity;

a7ae2d43fbd3713b9379ca8b61b6d9d444a9d127
Debanshu111 <debanshubiswas111@gmail.com>
2024-12-05 21:24:32 +0530

    function to generate objects and multiple obj.

--- javascript-amazon-project-main/data/cart-oop.js
+++ javascript-amazon-project-main/data/cart-oop.js
@@ -1,113 +1,122 @@
-//OOPS concept...Cart defined as an obj. to group all data and func into it
-//can't use keywords directly inside obj
-//fuction inside obj is called METHOD
-const cart = {
-    cartItems: undefined,
+//Funtion to create multiple obj.
+function Cart(localStorageKey) {  //localStorageKey is used so that different cart can use different parameter
+    //OOPS concept...Cart defined as an obj. to group all data and func into it
+    //can't use keywords directly inside obj
+    //fuction inside obj is called METHOD
+    const cart = {
+        cartItems: undefined,
 
-    //LOAD FROM STORAGE FUNCTION
-    loadFromStorage() {
-        this.cartItems = JSON.parse(localStorage.getItem("cart-oop"));
+        //LOAD FROM STORAGE FUNCTION
+        loadFromStorage() {
+            this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
 
-        if (!this.cartItems) {
-            this.cartItems = [
-                {
-                    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-                    quantity: 2,
-                    deliveryOptionId: "1",
-                },
-                {
-                    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-                    quantity: 1,
-                    deliveryOptionId: "2",
-                },
-            ];
-        }
-    },
-
-    //LOCAL STORAGE FUNCTION
-    saveToStorage() {
-        localStorage.setItem("cart-oop", JSON.stringify(this.cartItems)); //Saving to local storage(only saves strings, so cart has been converted to string)
-    },
-
-    //ADD TO CART FUNCTION
-    addToCart(productId) {
-        let matchingItem;
-        this.cartItems.forEach((cartItem) => {
-            if (productId === cartItem.productId) {
-                matchingItem = cartItem;
+            if (!this.cartItems) {
+                this.cartItems = [
+                    {
+                        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                        quantity: 2,
+                        deliveryOptionId: "1",
+                    },
+                    {
+                        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                        quantity: 1,
+                        deliveryOptionId: "2",
+                    },
+                ];
             }
-        });
+        },
 
-        //QUANTITY DROPDOWN
-        const quantitySelector = document.querySelector(
-            `.js-quantity-selector-${productId}`
-        );
-        const quantity = Number(quantitySelector?.value);
-        //Cart Item already in Cart?
-        if (matchingItem) {
-            matchingItem.quantity = matchingItem.quantity + quantity;
-        } else {
-            this.cartItems.push({
-                productId: productId,
-                quantity: quantity,
-                deliveryOptionId: "1", //used  1 for now...need to change later
+        //LOCAL STORAGE FUNCTION
+        saveToStorage() {
+            localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems)); //Saving to local storage(only saves strings, so cart has been converted to string)
+        },
+
+        //ADD TO CART FUNCTION
+        addToCart(productId) {
+            let matchingItem;
+            this.cartItems.forEach((cartItem) => {
+                if (productId === cartItem.productId) {
+                    matchingItem = cartItem;
+                }
             });
-        }
-        this.saveToStorage(); //since already inside the obj.
-    },
 
-    //REMOVE FROM CART FUNCTION
-    removeFromCart(productId) {
-        // let deletedProduct;
-        const newCart = []; //NEW CART for Deleted product check
-        this.cartItems.forEach((cartItem) => {
-            //To check whatever is in the cart we loop
-            if (cartItem.productId !== productId) {
-                newCart.push(cartItem); //new cart will be the one after checking delete option, we push cartItem into New Cart
+            //QUANTITY DROPDOWN
+            const quantitySelector = document.querySelector(
+                `.js-quantity-selector-${productId}`
+            );
+            const quantity = Number(quantitySelector?.value);
+            //Cart Item already in Cart?
+            if (matchingItem) {
+                matchingItem.quantity = matchingItem.quantity + quantity;
+            } else {
+                this.cartItems.push({
+                    productId: productId,
+                    quantity: quantity,
+                    deliveryOptionId: "1", //used  1 for now...need to change later
+                });
             }
-        });
-        this.cartItems = newCart; //post deletion check...cart is updated...since it's dependent on options lile delete...we change const to let for Cart at the beginning
-        this.saveToStorage();
-    },
+            this.saveToStorage(); //since already inside the obj.
+        },
 
-    //CALCULATE CART QUANTITY FUNCTION used at multiple files, to optiimize
-    calculateCartQuantity() {
-        let cartQuantity = 0;
-        this.cartItems.forEach((cartItem) => {
-            cartQuantity = cartQuantity + cartItem.quantity;
-        });
-        return cartQuantity;
-    },
+        //REMOVE FROM CART FUNCTION
+        removeFromCart(productId) {
+            // let deletedProduct;
+            const newCart = []; //NEW CART for Deleted product check
+            this.cartItems.forEach((cartItem) => {
+                //To check whatever is in the cart we loop
+                if (cartItem.productId !== productId) {
+                    newCart.push(cartItem); //new cart will be the one after checking delete option, we push cartItem into New Cart
+                }
+            });
+            this.cartItems = newCart; //post deletion check...cart is updated...since it's dependent on options lile delete...we change const to let for Cart at the beginning
+            this.saveToStorage();
+        },
 
-    //UPDATE QUANTITY FUNCTION
-    updateQuantity(productId, newQuantity) {
-        let matchingItem;
-        this.cartItems.forEach((cartItem) => {
-            if (productId === cartItem.productId) {
-                matchingItem = cartItem;
-            }
-        });
-        matchingItem.quantity = newQuantity;
-        this.saveToStorage();
-    },
+        //CALCULATE CART QUANTITY FUNCTION used at multiple files, to optiimize
+        calculateCartQuantity() {
+            let cartQuantity = 0;
+            this.cartItems.forEach((cartItem) => {
+                cartQuantity = cartQuantity + cartItem.quantity;
+            });
+            return cartQuantity;
+        },
 
-    //UPDATE DELIVERY OPTION FUNTION
-    updateDeliveryOption(productId, deliveryOptionId) {
-        //Match the delivery date selection as per checked in the radio options
-        let matchingItem;
-        this.cartItems.forEach((cartItem) => {
-            if (productId === cartItem.productId) {
-                matchingItem = cartItem;
-            }
-        });
+        //UPDATE QUANTITY FUNCTION
+        updateQuantity(productId, newQuantity) {
+            let matchingItem;
+            this.cartItems.forEach((cartItem) => {
+                if (productId === cartItem.productId) {
+                    matchingItem = cartItem;
+                }
+            });
+            matchingItem.quantity = newQuantity;
+            this.saveToStorage();
+        },
 
-        matchingItem.deliveryOptionId = deliveryOptionId;
-        this.saveToStorage();
-    }
-};
+        //UPDATE DELIVERY OPTION FUNTION
+        updateDeliveryOption(productId, deliveryOptionId) {
+            //Match the delivery date selection as per checked in the radio options
+            let matchingItem;
+            this.cartItems.forEach((cartItem) => {
+                if (productId === cartItem.productId) {
+                    matchingItem = cartItem;
+                }
+            });
+
+            matchingItem.deliveryOptionId = deliveryOptionId;
+            this.saveToStorage();
+        },
+    };
+
+    return cart;
+}
+
+const cart = Cart('cart-oop');
+const businessCart = Cart('cart-business');
 
 cart.loadFromStorage(); //since it's present inside the cart obj.
 
-cart.addToCart("83d4ca15-0f35-48f5-b7a3-1ea210004f2e");
+businessCart.loadFromStorage(); //since it's present inside the businessCart obj.
 
 console.log(cart);
+console.log(businessCart);
